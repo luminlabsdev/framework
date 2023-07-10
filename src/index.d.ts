656f35470e45162344d550416d7599b8f2bce2df
James <jimmidty.jammmers@gmail.com>
2023-07-10 17:05:05 -0500

    Add support for roblox-ts

---
+++ src/index.d.ts
@@ -0,0 +1,97 @@
+interface EngineServer {
+    Packages: {
+        Server: any
+        Replicated: any
+    }
+
+    Media: {
+        Server: any
+        Replicated: any
+    }
+
+	CreateNetworkController(controllerName: string): ServerNetworkController<any>;
+}
+
+interface EngineClient {
+    Packages: {
+        Client: any
+        Replicated: any
+    }
+
+    Media: {
+        Client: any
+        Replicated: any
+    }
+
+    Player: Player
+
+    PlayerGui: StarterGui
+    PlayerBackpack: StarterPack
+
+	CreateNetworkController(controllerName: string): ClientNetworkController<any>;
+}
+
+interface ScriptConnection {
+    Disconnect(): void;
+    Connected: boolean;
+}
+
+export interface ScriptSignal<T> {
+    Connect(func: (data: {T}) => (void)): ScriptConnection;
+    Wait(): {T};
+    Once(func: (data: {T}) => (void)): ScriptConnection;
+
+    Fire(data: (T | {T}) | null): void;
+    DisconnectAll(): void;
+
+    Name: string
+}
+
+export interface ClientNetworkController<T> {
+    Connect(self: ClientNetworkController<T>, func: (data: {T}) => (void)): ScriptConnection;
+    Wait(self: ClientNetworkController<T>): {T};
+    Once(self: ClientNetworkController<T>, func: (data: {T}) => (void)): ScriptConnection;
+
+    Fire(self: ClientNetworkController<T>, data: (T | {T}) | null): void;
+    InvokeAsync(self: ClientNetworkController<T>, data: (T | {T}) | null): {T};
+}
+
+export interface ServerNetworkController<T> {
+    Connect(self: ServerNetworkController<T>, func: (sender: Player, data: {T}) => (void)): ScriptConnection;
+    Wait(self: ServerNetworkController<T>): [Player, {T}];
+    Once(self: ServerNetworkController<T>, func: (data: {T}) => (void)): ScriptConnection;
+
+    Fire(self: ServerNetworkController<T>, recipient: any, data: (T | {T}) | null): void;
+    OnInvoke(self: ServerNetworkController<T>, callback: (sender: any, data: {T}) => void): void;
+}
+
+export namespace CanaryEngine {
+    export function GetEngineServer(): EngineServer;
+	export function GetEngineClient(): EngineClient;
+	export function CreateSignal(): ScriptSignal<any>;
+	export function GetLatestPackageVersionAsync(packageInstance: any, warnIfNotLatestVersion: boolean | null, respectDebugger: boolean | null): number | null;
+
+    const Runtime: {
+        RuntimeSettings: {
+            StudioDebugEnabled: boolean;
+            CheckLatestVersion: boolean;
+            LiveGameDebugger: boolean
+        };
+
+        RuntimeContext: {
+            Studio: boolean;
+            Server: boolean;
+            Client: boolean;
+            StudioPlay: boolean;
+        };
+    }
+
+    const Libraries: {
+        Utility: any
+        Benchmark: any
+        Statistics: any
+        Serialize: any
+    }
+	
+	let DeveloperMode: boolean
+}
