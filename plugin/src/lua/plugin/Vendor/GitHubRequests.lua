0b4e1e3813b6c543cc150d313521e4e00dceec37
James <jamesalexanderlittlefield@gmail.com>
2023-11-20 23:14:15 -0600

    push changes

--- plugin/src/lua/plugin/Vendor/GitHubRequests.lua
+++
@@ -1,5 +0,0 @@
-return {
-	ReleaseLatest = "https://api.github.com/repos/canary-development/CanaryEngine/releases/latest",
-	LibrariesList = "https://raw.githubusercontent.com/canary-development/CanaryEngine/main/plugin/src/json/librarieslist.json",
-	ExternalSettings = "https://raw.githubusercontent.com/canary-development/CanaryEngine/main/plugin/settings/settings.json"
-}
