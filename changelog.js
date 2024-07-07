73d3f8b03b8740f227bc3eb42c8e0d5244d9d0d8
James <119134081+2jammers@users.noreply.github.com>
2024-07-06 22:22:48 -0500

    gets previous version

--- changelog.js
+++ changelog.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const https = require('https');
 
 const VERSION = process.argv[2].replace(/^v/, '');
-const PREVIOUS_VERSION = process.argv[3];
 const CHANGELOG_FILE = 'docs/changelog.md';
 const USER = 'lumin-dev'
 const PROJECT = 'LuminFramework'
@@ -31,12 +30,38 @@ const fetchQuote = () => {
   });
 };
 
+const fetchPreviousTag = async () => {
+  try {
+    const response = await axios.get(`https://api.github.com/repos/${USER}/${PROJECT}/releases`, {
+      headers: {
+        Authorization: `token YOUR_GITHUB_TOKEN`,
+        'Content-Type': 'application/json',
+      },
+      params: {
+        per_page: 2,
+      },
+    });
+
+    const releases = response.data;
+    if (releases.length >= 2) {
+      const previousTag = releases[1].tag_name;
+      return previousTag;
+    } else {
+      return 'main';
+    }
+  } catch (error) {
+    console.error('Error fetching previous tag:', error);
+    return null;
+  }
+};
+
 const createReleaseNotes = async (version) => {
   try {
     const data = await fetchQuote();
     const changelog = extractChangelog(version);
+    const previousVersion = await fetchPreviousTag();
     if (changelog) {
-      const releaseNotes = `**${data[0]} - ${data[1]}**\n\n${changelog}\n\n**Internal changes:** https://github.com/${USER}/${PROJECT}/compare/${PREVIOUS_VERSION}...v${VERSION}`;
+      const releaseNotes = `**${data[0]} - ${data[1]}**\n\n${changelog}\n\n**Internal changes:** https://github.com/${USER}/${PROJECT}/compare/${previousVersion}...v${VERSION}`;
       fs.writeFileSync('release_log.md', releaseNotes);
       console.log('Release notes created successfully.');
     } else {
